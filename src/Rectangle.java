653a38779a764455402005c2a9d7d23e7e6eb983
Athanasios Lagopoulos <sakrifor@gmail.com>
2019-10-06 12:16:49 +0300

    Small grammar fixes

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -6,10 +6,10 @@ public class Rectangle {
 
     /*
     O κατασκευαστής θα δέχεται δύο παραμέτρους. Η πρώτη
-    παράμετρος θα αφορά το μήκος και η δεύτερη το πλάτος του ορθογωνίου.
+    παράμετρος θα αφορά το πλάτος και η δεύτερη το ύψος του ορθογωνίου.
 
     The constructor should have 2 parameters. The first parameter
-    should be the length of the rectangle and the second one its width.
+    should be the width of the rectangle and the second one its height.
     */
 
     /* Μέθοδος που θα επιστρέφει την περίμετρο του ορθογωνίου
@@ -23,23 +23,23 @@ public class Rectangle {
     public double getArea() {
     }
 
-    /* Μέθοδος που αλλάζει το μήκος του ορθογωνίου
-     * This method should change the length of the rectangle */
+    /* Μέθοδος που αλλάζει το πλάτος του ορθογωνίου
+     * This method should change the width of the rectangle */
     public void setWidth(double x) {
     }
 
-    /* Μέθοδος που αλλάζει το πλάτος του ορθογωνίου
-     * This method should change the width of the rectangle */
+    /* Μέθοδος που αλλάζει το ύψος του ορθογωνίου
+     * This method should change the height of the rectangle */
     public void setHeight(double x) {
     }
 
-    /* Μέθοδος που επιστρέφει το μήκος του ορθογωνίου
-     * This method should return the length of the rectangle */
+    /* Μέθοδος που επιστρέφει το πλάτος του ορθογωνίου
+     * This method should return the width of the rectangle */
     public double getWidth() {
     }
 
-    /* Μέθοδος που επιστρέφει το πλάτος του ορθογωνίου
-     * This method should return the width of the rectangle*/
+    /* Μέθοδος που επιστρέφει το ύψος του ορθογωνίου
+     * This method should return the height of the rectangle*/
     public double getHeight() {
 
     }
